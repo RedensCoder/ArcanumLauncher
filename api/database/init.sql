43c64591faac5b4146d6032d28fedd355f57693b
RedensCoder <andreysidorov04@mail.ru>
2023-05-31 16:36:40 +0300

    init updtae

--- api/database/init.sql
+++ api/database/init.sql
@@ -2,5 +2,9 @@ CREATE TABLE IF NOT EXISTS public.users
 (
     username text COLLATE pg_catalog."default" NOT NULL,
     password text COLLATE pg_catalog."default" NOT NULL,
+    email text COLLATE pg_catalog."default" NOT NULL,
+    about text COLLATE pg_catalog."default" NOT NULL,
+    avatar text COLLATE pg_catalog."default" NOT NULL,
+    lvl integer COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (username)
 )
