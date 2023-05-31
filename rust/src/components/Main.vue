c4cbc28d0e4aa7b12c9aa0f4ddbc191f8b2a9d6b
KupliN1 <kirill.chernyav.2004@gmail.com>
2023-05-31 21:39:26 +0300

    DA

--- rust/src/components/Main.vue
+++ rust/src/components/Main.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>Adrian Lox</h1>
+    <div class="header">
+        <router-link to="/" class="text1"><h2>ARCANUM LAUNCHER</h2></router-link>
+    </div>
+
+
 </template>
 
 <script setup>
@@ -7,5 +11,22 @@
 </script>
 
 <style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Concert+One&family=Russo+One&display=swap');
 
+    .header {
+        background-color: #393E46;
+        width: 100%;
+        padding: 20px;
+    }
+
+    .text1 {
+        margin: 20px 0;
+        text-decoration: none;
+        font-family: 'Concert One';
+        font-style: normal;
+        font-size: 48px;
+        line-height: 37px;
+        color: white;
+        /* text-align: center; */
+    }
 </style>
